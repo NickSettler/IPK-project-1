d8de11b696e51a679dafbf769d72cfecf706e270
NickSettler <moiseevnikita14@gmail.com>
2023-02-16 21:53:26 +0100

    refactor: add cmake and main executable file

---
+++ src/client.cpp
@@ -0,0 +1,7 @@
+/**
+ * 
+ * @file: client.cpp
+ * @date: 16.02.2023
+ */
+
+#include "client.h"
---
+++ src/client.h
@@ -0,0 +1,10 @@
+/**
+ * 
+ * @file: client.h
+ * @date: 16.02.2023
+ */
+
+#ifndef IFJ_PROJ_CLIENT_H
+#define IFJ_PROJ_CLIENT_H
+
+#endif //IFJ_PROJ_CLIENT_H

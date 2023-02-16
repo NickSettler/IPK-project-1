f23890405e8aad67cb895d7ceda4727663f6b5a0
NickSettler <moiseevnikita14@gmail.com>
2023-02-16 22:58:43 +0100

    feat(arguments): add arguments namespace
    * add argument type with flag and value
    * add arguments parsing class
    * add not allowed flag exception

---
+++ src/args.cpp
@@ -0,0 +1,94 @@
+/**
+ * Arguments parser file
+ *
+ * @file: args.cpp
+ * @date: 16.02.2023
+ */
+
+#include "args.h"
+
+arguments::NotAllowedArgumentException::NotAllowedArgumentException(char flag) {
+    this->flag = new char[2];
+    this->flag[0] = flag;
+    this->flag[1] = '\0';
+}
+
+char *arguments::NotAllowedArgumentException::what() {
+    if (this->flag == nullptr) { return (char *) "Flag is not allowed."; }
+
+    auto *message = new std::string("Flag ");
+    message->append(this->flag);
+    message->append(" is not allowed.");
+
+    return (char *) message->c_str();
+}
+
+arguments::ArgumentsParser::ArgumentsParser(int argc, char **argv) : argc(argc), argv(argv) {
+    this->args = new std::vector<program_arg_t>();
+}
+
+arguments::ArgumentsParser::ArgumentsParser(int argc, char **argv, std::vector<char> allowed_flags)
+    : argc(argc), argv(argv), allowed_flags(new std::vector<char>(std::move(allowed_flags))) {
+    this->args = new std::vector<program_arg_t>();
+}
+
+arguments::ArgumentsParser::~ArgumentsParser() {
+    this->args->clear();
+    delete this->args;
+
+    if (this->allowed_flags != nullptr) {
+        this->allowed_flags->clear();
+        delete this->allowed_flags;
+    }
+}
+
+void arguments::ArgumentsParser::parse() {
+    for (int i = 1; i < this->argc; i++) {
+        if (this->argv[i][0] == '-') {
+            /*
+             * Check if flag is allowed. If allowed_flags is nullptr, all flags are allowed.
+             * Otherwise, check if flag is in allowed_flags.
+             */
+            auto is_flag_allowed = this->allowed_flags == nullptr ||
+                                   (this->allowed_flags != nullptr &&
+                                    std::find(this->allowed_flags->begin(), this->allowed_flags->end(),
+                                              this->argv[i][1]) != this->allowed_flags->end());
+            if (is_flag_allowed) {
+                /*
+                 * Parse argument. Example:
+                 * { "-h", "1.2.3.4" } -> arg: (program_arg_t){ flag: 'h', value: "1.2.3.4" }
+                 * this->args = { ..., arg }
+                 */
+                program_arg_t arg;
+                arg.flag = this->argv[i][1];
+                arg.value = this->argv[i + 1];
+                this->args->push_back(arg);
+            } else {
+                throw NotAllowedArgumentException(this->argv[i][1]);
+            }
+        }
+    }
+}
+
+std::string arguments::ArgumentsParser::get_value(char flag) {
+    for (auto &arg: *this->args) {
+        if (arg.flag == flag) { return arg.value; }
+    }
+
+    return "";
+}
+
+bool arguments::ArgumentsParser::has_flag(char flag) {
+    return std::any_of(this->args->begin(), this->args->end(),
+                       [flag](const program_arg_t &arg) { return arg.flag == flag; });
+}
+
+std::vector<arguments::program_arg_t> *arguments::ArgumentsParser::get_args() { return this->args; }
+
+void arguments::ArgumentsParser::print_help() {
+    printf("Usage: ipkcpc -h <host> -p <port> -m <mode>\n");
+    printf("Options:\n");
+    printf("  -h <host>    IPv4 address of the server\n");
+    printf("  -p <port>    Port number of the server\n");
+    printf("  -m <mode>    Mode of the client (TCP or UDP)\n");
+}
---
+++ src/args.h
@@ -0,0 +1,136 @@
+/**
+ * Arguments parser file
+ *
+ * @file: args.h
+ * @date: 16.02.2023
+ */
+
+#ifndef IPKCPC_ARGS_H
+#define IPKCPC_ARGS_H
+
+#include <string>
+#include <vector>
+#include <utility>
+
+/**
+ * Arguments namespace
+ *
+ * @namespace arguments
+ */
+namespace arguments {
+    /**
+     * Program argument type
+     *
+     * @typedef program_arg_t
+     * @struct program_arg
+     * @var program_arg::flag
+     * Flag of the argument
+     * @var program_arg::value
+     * Value of the argument
+     */
+    typedef struct program_arg {
+        char flag;
+        std::string value;
+    } program_arg_t;
+
+    /**
+     * Not allowed argument exception class
+     *
+     * @class NotAllowedArgumentException
+     */
+    class NotAllowedArgumentException : public std::exception {
+    private:
+        /** @var flag Flag of the argument */
+        char *flag = nullptr;
+
+    public:
+        /**
+         * NotAllowedArgumentException constructor
+         *
+         * @param flag Flag of the argument
+         */
+        explicit NotAllowedArgumentException(char flag);
+
+        /**
+         * Cause of the exception
+         */
+        char *what();
+    };
+
+    /**
+     * Arguments parser class
+     *
+     * @class ArgumentsParser
+     */
+    class ArgumentsParser {
+    private:
+        /** @var argc Program argument count */
+        int argc;
+        /** @var argv Program arguments array */
+        char **argv;
+        /** @var allowed_flags Allowed flags */
+        std::vector<char> *allowed_flags = nullptr;
+
+        /** @var args Vector of parsed arguments */
+        std::vector<program_arg_t> *args;
+
+    public:
+        /**
+         * ArgumentsParser constructor without allowed flags
+         * Allowed flags are set to default (nullptr), so all flags are allowed
+         *
+         * @param argc Program argument count
+         * @param argv Program arguments array
+         */
+        ArgumentsParser(int argc, char **argv);
+
+        /**
+         * ArgumentsParser constructor with allowed flags
+         * Allowed flags are set to passed vector of allowed flags, so only flags from this vector are allowed
+         *
+         * @param argc Program argument count
+         * @param argv Program arguments array
+         * @param allowed_flags Vector of allowed flags
+         */
+        ArgumentsParser(int argc, char **argv, std::vector<char> allowed_flags);
+
+        /**
+         * ArgumentsParser destructor
+         */
+        ~ArgumentsParser();
+
+        /**
+         * Parse arguments
+         */
+        void parse();
+
+        /**
+         * Get value of the argument with specified flag
+         *
+         * @param flag Flag of the argument
+         * @return Value of the argument
+         */
+        std::string get_value(char flag);
+
+        /**
+         * Check if argument with specified flag exists
+         *
+         * @param flag Flag of the argument
+         * @return True if argument with specified flag exists, false otherwise
+         */
+        bool has_flag(char flag);
+
+        /**
+         * Get vector of parsed arguments
+         * @return Vector of parsed arguments
+         */
+        std::vector<program_arg_t> *get_args();
+
+        /**
+         * Print help. This method is called when unknown flag is passed to the program
+         */
+        static void print_help();
+    };
+}// namespace arguments
+
+#endif// IPKCPC_ARGS_H

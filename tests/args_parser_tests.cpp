227f889959e654484f2970570cd88e76f23ae113
NickSettler <moiseevnikita14@gmail.com>
2023-02-16 23:11:27 +0100

    chore: update project CPP standard to 20

--- tests/args_parser_tests.cpp
+++ tests/args_parser_tests.cpp
@@ -11,139 +11,135 @@
 #include "../src/args.h"
 #include "../src/args.cpp"
 
-namespace IPK {
-    namespace tests {
-        namespace {
-            class ArgumentsParserTests : public ::testing::Test {
-            protected:
-                arguments::ArgumentsParser *args_parser{};
+namespace IPK::tests {
+    namespace {
+        class ArgumentsParserTests : public ::testing::Test {
+        protected:
+            arguments::ArgumentsParser *args_parser{};
 
-                void Init(int argc, char **argv) {
-                    this->args_parser = new arguments::ArgumentsParser(argc, argv);
-                    this->args_parser->parse();
-                }
-
-                void Init(int argc, char **argv, std::vector<char> allowed_flags) {
-                    this->args_parser = new arguments::ArgumentsParser(argc, argv, std::move(allowed_flags));
-                    this->args_parser->parse();
-                }
-
-                void TearDown() override { delete this->args_parser; }
-
-                void CheckArgument(char flag, const std::string &value) {
-                    EXPECT_EQ(true, this->args_parser->has_flag(flag));
-                    EXPECT_EQ(value, this->args_parser->get_value(flag));
-                }
-
-                void CheckArguments(const std::vector<arguments::program_arg_t> &args) {
-                    EXPECT_EQ(args.size(), this->args_parser->get_args()->size());
-
-                    for (auto &arg: args) { CheckArgument(arg.flag, arg.value); }
-                }
-            };
-
-            TEST_F(ArgumentsParserTests, OneHostArgument) {
-                int argc = 3;
-                char *argv[] = {(char *) "client", (char *) "-h", (char *) "1.2.3.4"};
-
-                Init(argc, argv);
-
-                CheckArguments({{.flag = 'h', .value = "1.2.3.4"}});
+            void Init(int argc, char **argv) {
+                this->args_parser = new arguments::ArgumentsParser(argc, argv);
+                this->args_parser->parse();
             }
 
-            TEST_F(ArgumentsParserTests, OnePortArgument) {
-                int argc = 3;
-                char *argv[] = {(char *) "client", (char *) "-p", (char *) "1234"};
-
-                Init(argc, argv);
-
-                CheckArguments({{.flag = 'p', .value = "1234"}});
+            void Init(int argc, char **argv, std::vector<char> allowed_flags) {
+                this->args_parser = new arguments::ArgumentsParser(argc, argv, std::move(allowed_flags));
+                this->args_parser->parse();
             }
 
-            TEST_F(ArgumentsParserTests, OneModeArgument) {
-                int argc = 3;
-                char *argv[] = {(char *) "client", (char *) "-m", (char *) "tcp"};
+            void TearDown() override { delete this->args_parser; }
 
-                Init(argc, argv);
-
-                CheckArguments({{.flag = 'm', .value = "tcp"}});
+            void CheckArgument(char flag, const std::string &value) {
+                EXPECT_EQ(true, this->args_parser->has_flag(flag));
+                EXPECT_EQ(value, this->args_parser->get_value(flag));
             }
 
-            TEST_F(ArgumentsParserTests, HostAndPortArguments) {
-                int argc = 5;
-                char *argv[] = {(char *) "client", (char *) "-h", (char *) "1.2.3.4", (char *) "-p", (char *) "1234"};
+            void CheckArguments(const std::vector<arguments::program_arg_t> &args) {
+                EXPECT_EQ(args.size(), this->args_parser->get_args()->size());
 
-                Init(argc, argv);
-
-                CheckArguments({{.flag = 'h', .value = "1.2.3.4"}, {.flag = 'p', .value = "1234"}});
+                for (auto &arg: args) { CheckArgument(arg.flag, arg.value); }
             }
+        };
 
-            TEST_F(ArgumentsParserTests, HostAndPortAndModeArguments) {
-                int argc = 7;
-                char *argv[] = {(char *) "client", (char *) "-h", (char *) "1.2.3.4", (char *) "-p",
-                                (char *) "1234",   (char *) "-m", (char *) "tcp"};
+        TEST_F(ArgumentsParserTests, OneHostArgument) {
+            int argc = 3;
+            char *argv[] = {(char *) "client", (char *) "-h", (char *) "1.2.3.4"};
 
-                Init(argc, argv);
+            Init(argc, argv);
 
-                CheckArguments({{.flag = 'h', .value = "1.2.3.4"},
-                                {.flag = 'p', .value = "1234"},
-                                {.flag = 'm', .value = "tcp"}});
-            }
+            CheckArguments({{.flag = 'h', .value = "1.2.3.4"}});
+        }
 
-            TEST_F(ArgumentsParserTests, AllowedOnlyHostArgument) {
-                int argc = 3;
-                char *argv[] = {(char *) "client", (char *) "-h", (char *) "1.2.3.4"};
+        TEST_F(ArgumentsParserTests, OnePortArgument) {
+            int argc = 3;
+            char *argv[] = {(char *) "client", (char *) "-p", (char *) "1234"};
 
-                Init(argc, argv, {'h'});
+            Init(argc, argv);
 
-                CheckArguments({{.flag = 'h', .value = "1.2.3.4"}});
-            }
+            CheckArguments({{.flag = 'p', .value = "1234"}});
+        }
 
-            TEST_F(ArgumentsParserTests, AllowedOnlyPortArgument) {
-                int argc = 3;
-                char *argv[] = {(char *) "client", (char *) "-p", (char *) "1234"};
+        TEST_F(ArgumentsParserTests, OneModeArgument) {
+            int argc = 3;
+            char *argv[] = {(char *) "client", (char *) "-m", (char *) "tcp"};
 
-                Init(argc, argv, {'p'});
+            Init(argc, argv);
 
-                CheckArguments({{.flag = 'p', .value = "1234"}});
-            }
+            CheckArguments({{.flag = 'm', .value = "tcp"}});
+        }
 
-            TEST_F(ArgumentsParserTests, AllowedOnlyModeArgument) {
-                int argc = 3;
-                char *argv[] = {(char *) "client", (char *) "-m", (char *) "tcp"};
+        TEST_F(ArgumentsParserTests, HostAndPortArguments) {
+            int argc = 5;
+            char *argv[] = {(char *) "client", (char *) "-h", (char *) "1.2.3.4", (char *) "-p", (char *) "1234"};
 
-                Init(argc, argv, {'m'});
+            Init(argc, argv);
 
-                CheckArguments({{.flag = 'm', .value = "tcp"}});
-            }
+            CheckArguments({{.flag = 'h', .value = "1.2.3.4"}, {.flag = 'p', .value = "1234"}});
+        }
 
-            TEST_F(ArgumentsParserTests, AllowedAllArguments) {
-                int argc = 7;
-                char *argv[] = {(char *) "client", (char *) "-h", (char *) "1.2.3.4", (char *) "-p",
-                                (char *) "1234",   (char *) "-m", (char *) "tcp"};
+        TEST_F(ArgumentsParserTests, HostAndPortAndModeArguments) {
+            int argc = 7;
+            char *argv[] = {(char *) "client", (char *) "-h", (char *) "1.2.3.4", (char *) "-p",
+                            (char *) "1234",   (char *) "-m", (char *) "tcp"};
 
-                Init(argc, argv, {'h', 'p', 'm'});
+            Init(argc, argv);
 
-                CheckArguments({{.flag = 'h', .value = "1.2.3.4"},
-                                {.flag = 'p', .value = "1234"},
-                                {.flag = 'm', .value = "tcp"}});
-            }
+            CheckArguments(
+                    {{.flag = 'h', .value = "1.2.3.4"}, {.flag = 'p', .value = "1234"}, {.flag = 'm', .value = "tcp"}});
+        }
 
-            TEST_F(ArgumentsParserTests, PassedUnallowedArgument) {
-                int argc = 3;
-                char *argv[] = {(char *) "client", (char *) "-h", (char *) "1.2.3.4"};
+        TEST_F(ArgumentsParserTests, AllowedOnlyHostArgument) {
+            int argc = 3;
+            char *argv[] = {(char *) "client", (char *) "-h", (char *) "1.2.3.4"};
 
-                EXPECT_THROW(Init(argc, argv, {}), arguments::NotAllowedArgumentException);
-            }
+            Init(argc, argv, {'h'});
 
-            TEST_F(ArgumentsParserTests, CheckNonexistantArgument) {
-                int argc = 3;
-                char *argv[] = {(char *) "client", (char *) "-h", (char *) "1.2.3.4"};
+            CheckArguments({{.flag = 'h', .value = "1.2.3.4"}});
+        }
 
-                Init(argc, argv);
+        TEST_F(ArgumentsParserTests, AllowedOnlyPortArgument) {
+            int argc = 3;
+            char *argv[] = {(char *) "client", (char *) "-p", (char *) "1234"};
 
-                EXPECT_EQ(false, this->args_parser->has_flag('p'));
-            }
-        }// namespace
-    }    // namespace tests
-}// namespace IPK
+            Init(argc, argv, {'p'});
+
+            CheckArguments({{.flag = 'p', .value = "1234"}});
+        }
+
+        TEST_F(ArgumentsParserTests, AllowedOnlyModeArgument) {
+            int argc = 3;
+            char *argv[] = {(char *) "client", (char *) "-m", (char *) "tcp"};
+
+            Init(argc, argv, {'m'});
+
+            CheckArguments({{.flag = 'm', .value = "tcp"}});
+        }
+
+        TEST_F(ArgumentsParserTests, AllowedAllArguments) {
+            int argc = 7;
+            char *argv[] = {(char *) "client", (char *) "-h", (char *) "1.2.3.4", (char *) "-p",
+                            (char *) "1234",   (char *) "-m", (char *) "tcp"};
+
+            Init(argc, argv, {'h', 'p', 'm'});
+
+            CheckArguments(
+                    {{.flag = 'h', .value = "1.2.3.4"}, {.flag = 'p', .value = "1234"}, {.flag = 'm', .value = "tcp"}});
+        }
+
+        TEST_F(ArgumentsParserTests, PassedUnallowedArgument) {
+            int argc = 3;
+            char *argv[] = {(char *) "client", (char *) "-h", (char *) "1.2.3.4"};
+
+            EXPECT_THROW(Init(argc, argv, {}), arguments::NotAllowedArgumentException);
+        }
+
+        TEST_F(ArgumentsParserTests, CheckNonexistantArgument) {
+            int argc = 3;
+            char *argv[] = {(char *) "client", (char *) "-h", (char *) "1.2.3.4"};
+
+            Init(argc, argv);
+
+            EXPECT_EQ(false, this->args_parser->has_flag('p'));
+        }
+    }// namespace
+}// namespace IPK::tests
